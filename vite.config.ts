b50f83f99be20df21cce17c2a9dae3ea65caf187
Eduard Baron <ebaron@modyo.com>
2025-10-10 11:18:17 -0500

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    svgr(),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@dynamic-framework/ui-react':
+        path.resolve(__dirname, 'node_modules/@dynamic-framework/ui-react'),
+      'node_modules/bootstrap':
+        path.resolve(__dirname, 'node_modules/bootstrap'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+        silenceDeprecations: ['legacy-js-api'],
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    minify: 'esbuild',
+    outDir: 'build',
+    assetsDir: '',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'main.js',
+        chunkFileNames: '[name].[hash].chunk.js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.names.includes('.css')) {
+            return 'main.css'
+          }
+          return '[name].[ext]'
+        },
+        format: 'es'
+      }
+    }
+  },
+});
